c4ec17a8876b28f8e200b140ac080da889972d02
Matthias Kretz <m.kretz@gsi.de>
2023-01-09 14:54:13 +0100

    Improved interface
    ChangeLog:
    
    	* iterator.h:
    	* simd2.h:

--- iterator.h
+++ iterator.h
@@ -70,11 +70,11 @@ namespace std::experimental
 
       constexpr friend difference_type
       operator-(simd_iterator it, simd_iterator_sentinel)
-      { return offset - difference_type(V::size()); }
+      { return it.offset - difference_type(V::size()); }
 
       constexpr friend difference_type
       operator-(simd_iterator_sentinel, simd_iterator it)
-      { return difference_type(V::size()) - offset; }
+      { return difference_type(V::size()) - it.offset; }
 
       constexpr friend simd_iterator
       operator+(difference_type x, const simd_iterator& it)
@@ -131,81 +131,81 @@ namespace std::experimental
       using iterator_category = std::random_access_iterator_tag;
       using difference_type = int;
 
-      constexpr simd_iterator() = default;
+      constexpr simd_mask_iterator() = default;
 
       constexpr
-      simd_iterator(const V& d, int x)
+      simd_mask_iterator(const V& d, int x)
       : data(&d), offset(x)
       {}
 
       constexpr
-      simd_iterator(const simd_iterator &) = default;
+      simd_mask_iterator(const simd_mask_iterator &) = default;
 
-      constexpr simd_iterator&
-      operator=(const simd_iterator &) = default;
+      constexpr simd_mask_iterator&
+      operator=(const simd_mask_iterator &) = default;
 
       constexpr value_type
       operator*() const
       { return (*data)[offset]; }
 
-      constexpr simd_iterator&
+      constexpr simd_mask_iterator&
       operator++()
       {
         ++offset;
         return *this;
       }
 
-      constexpr simd_iterator
+      constexpr simd_mask_iterator
       operator++(int)
       {
-        simd_iterator r = *this;
+        simd_mask_iterator r = *this;
         ++offset;
         return r;
       }
 
-      constexpr simd_iterator&
+      constexpr simd_mask_iterator&
       operator--()
       {
         --offset;
         return *this;
       }
 
-      constexpr simd_iterator
+      constexpr simd_mask_iterator
       operator--(int)
       {
-        simd_iterator r = *this;
+        simd_mask_iterator r = *this;
         --offset;
         return r;
       }
 
       constexpr difference_type
-      operator-(simd_iterator rhs) const
+      operator-(simd_mask_iterator rhs) const
       { return offset - rhs.offset; }
 
-      constexpr friend simd_iterator
-      operator+(difference_type x, const simd_iterator& it)
-      { return simd_iterator(*it.data, it.offset + x); }
+      constexpr friend simd_mask_iterator
+      operator+(difference_type x, const simd_mask_iterator& it)
+      { return simd_mask_iterator(*it.data, it.offset + x); }
 
-      constexpr friend simd_iterator
-      operator+(const simd_iterator& it, difference_type x)
-      { return simd_iterator(*it.data, it.offset + x); }
+      constexpr friend simd_mask_iterator
+      operator+(const simd_mask_iterator& it, difference_type x)
+      { return simd_mask_iterator(*it.data, it.offset + x); }
 
-      constexpr friend simd_iterator
-      operator-(difference_type x, const simd_iterator& it)
-      { return simd_iterator(*it.data, it.offset - x); }
+      constexpr friend simd_mask_iterator
+      operator-(difference_type x, const simd_mask_iterator& it)
+      { return simd_mask_iterator(*it.data, it.offset - x); }
 
-      constexpr friend simd_iterator
-      operator-(const simd_iterator& it, difference_type x)
-      { return simd_iterator(*it.data, it.offset - x); }
+      constexpr friend simd_mask_iterator
+      operator-(const simd_mask_iterator& it, difference_type x)
+      { return simd_mask_iterator(*it.data, it.offset - x); }
 
-      constexpr simd_iterator&
+      constexpr simd_mask_iterator&
       operator+=(difference_type x)
       {
         offset += x;
         return *this;
       }
 
-      constexpr simd_iterator&
+      constexpr simd_mask_iterator&
       operator-=(difference_type x)
       {
         offset -= x;
@@ -216,12 +216,12 @@ namespace std::experimental
       operator[](difference_type i) const
       { return (*data)[offset + i]; }
 
-      constexpr friend auto operator<=>(simd_iterator a, simd_iterator b)
+      constexpr friend auto operator<=>(simd_mask_iterator a, simd_mask_iterator b)
       { return a.offset <=> b.offset; }
 
-      constexpr friend bool operator==(simd_iterator a, simd_iterator b) = default;
+      constexpr friend bool operator==(simd_mask_iterator a, simd_mask_iterator b) = default;
 
-      constexpr friend bool operator==(simd_iterator a, simd_iterator_sentinel b)
+      constexpr friend bool operator==(simd_mask_iterator a, simd_iterator_sentinel b)
       { return a.offset == difference_type(V::size()); }
     };
 
--- simd2.h
+++ simd2.h
@@ -2,6 +2,22 @@
 #include <span>
 #include <ranges>
 
+namespace detail
+{
+  template <std::ranges::sized_range T>
+    constexpr inline std::size_t
+    static_range_size = []() {
+      if constexpr (requires { {T::size()} -> std::integral; })
+        return T::size();
+      else if constexpr (requires { {T::extent} -> std::integral; })
+        return T::extent;
+      else if constexpr (requires { {std::tuple_size_v<T>} -> std::integral; })
+        return std::tuple_size_v<T>;
+      else
+        return std::dynamic_extent;
+    }();
+}
+
 template <class T, class A>
 class simd2 : public stdx::simd<T, A>
 {
@@ -9,38 +25,47 @@ class simd2 : public stdx::simd<T, A>
   public:
     using stdx::simd<T, A>::simd;
 
-    constexpr simd2(const Base& b)
+    static inline constexpr std::integral_constant<std::size_t, Base::size()>
+      size = {};
+
+    constexpr
+    simd2(const Base& b)
     : Base(b)
     {}
 
     // construction from span is simple
-    constexpr simd2(std::span<T, Base::size()> mem)
+    constexpr
+    simd2(std::span<T, size()> mem)
     : Base(mem.data(), stdx::element_aligned)
     {}
 
     // ranges typically don't have a static size() function :(
     // but if one does, this ctor is useful
     template <std::ranges::contiguous_range R>
-      requires (std::same_as<std::ranges::range_value_t<R>, T> && R::size() == Base::size())
-      constexpr simd2(const R& range)
+      requires (std::same_as<std::ranges::range_value_t<R>, T>
+                  && detail::static_range_size<R> == size())
+      constexpr
+      simd2(const R& range)
       : Base(std::ranges::data(range), stdx::element_aligned)
       {}
 
-    template <std::ranges::input_range R>
-      explicit (std::same_as<std::ranges::range_value_t<R>, T>)
-      constexpr simd2(const R& range)
-      : Base(std::ranges::data(range), stdx::element_aligned)
+    template <std::ranges::random_access_range R>
+      requires(std::convertible_to<std::ranges::range_value_t<R>, T>)
+      constexpr explicit (not std::same_as<std::ranges::range_value_t<R>, T>)
+      simd2(const R& range)
+      : Base([&range](auto i) -> T { return range[i]; })
       {}
 
-
-    std::array<T, Base::size()> to_array() const noexcept
+    constexpr std::array<T, size()>
+    to_array() const noexcept
     {
-      std::array<T, Base::size()> r;
+      std::array<T, size()> r = {};
       this->copy_to(r.data(), stdx::element_aligned);
       return r;
     }
 
-    explicit operator std::array<T, Base::size()>() const noexcept
+    explicit
+    operator std::array<T, size()>() const noexcept
     { return to_array(); }
 };
 
